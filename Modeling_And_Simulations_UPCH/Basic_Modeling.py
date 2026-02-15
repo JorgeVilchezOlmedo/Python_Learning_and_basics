0b47faa16d0bbb0174cd516a0af0216d7afa0ffa
Jorge Vilchez <jvilchezolmedo@gmail.com>
2026-02-15 21:45:19 +0000

    First_TRy

---
+++ Modeling_And_Simulations_UPCH/Basic_Modeling.py
@@ -0,0 +1,7 @@
+#ayaya
+cd /workspaces/Python_Learning_and_basics
+chmod +x Modeling_And_Simulations_UPCH/Basic_Modeling.py
+ls -l Modeling_And_Simulations_UPCH/Basic_Modeling.py
+python3 
+print ("hola")
+exit()
